f41e052ac5574879218acfb461978b6c7a77df7f
osalyousef <os.forwork1@gmail.com>
2024-07-06 16:46:34 +0300

    Initial Commit

---
+++ betterRest/betterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterRestApp.swift
+//  betterRest
+//
+//  Created by Osamah alyousef on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct betterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
